3402cef71b2e63191f3138ae358b53112f8365e6
myg Xiang <1479627443@qq.com>
2026-03-12 14:34:03 +0800

    重构文件夹结构

---
+++ real_robot_control/robot_test/upper_receive.py
@@ -0,0 +1,40 @@
+#在ubuntu上位机中，接收windows下发送的trigger，并执行相应的动作
+import socket
+import os
+
+HOST = '0.0.0.0'
+PORT = 60000
+
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen(1)
+
+    print(f"Server started on {HOST}:{PORT}")
+
+    while True:
+        conn, addr = server.accept()
+        print(f"Connected by {addr}")
+
+        while True:
+            data = conn.recv(1024).decode('utf-8')
+            if not data:
+                break
+            print(f"Received data: {data}")
+
+            if data == "SHOULDER_UP":
+                print("SHOULDER_UP")
+                cmd = "/bin/bash -c 'cd ~/robot_cook && source devel/setup.bash && rosrun cook_cartesian moveit_trajectory_recorder.py --play ~/moveit_trajectories/ catch_position_001.yaml --arm left_arm'"
+                os.system(cmd)
+            
+            else:
+                print(f"Unknown command: {data}")
+
+        conn.close()
+        print(f"Connection closed by {addr}")
+
+if __name__ == '__main__':
+    start_server()
+
+        
--- real_robot_control/robot_test/win_publish.py
+++ real_robot_control/robot_test/win_publish.py
@@ -1,7 +1,7 @@
-#在windows下，发送trigger到listener_upper.py，验证和上位机通信是否成功
+#在windows中发送trigger，尝试让机械臂执行动作
 import socket
 import time
-UBUNTU_IP = '10.70.27.214' 
+UBUNTU_IP = '192.168.123.55' 
 PORT = 60000
 
 def send_trigger(trigger_msg):
@@ -20,22 +20,13 @@ def send_trigger(trigger_msg):
     except Exception as e:
         print(f"error: {e}")
     finally:
-
         if client:
             client.close()
 
+#测试执行大臂向上动作,等待8秒后执行复位动作
 if __name__ == '__main__':
-    
     send_trigger("SHOULDER_UP")
-    time.sleep(1)
-    
-    send_trigger("SHOULDER_DOWN")
-    time.sleep(1)
-    
-    send_trigger("ELBOW_UP")
-    time.sleep(1)
-    
-    send_trigger("ELBOW_DOWN")
-    time.sleep(1)
-    
-    send_trigger("WAITING")
+    print("SHOULDER_UP sent")
+    time.sleep(8)
+
+    send_trigger("RESET")
